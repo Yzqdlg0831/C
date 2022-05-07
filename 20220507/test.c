fa265427a53e2ab463c9c5b7c16b8bd75900a880
whpucy <1980697614@qq.com>
2022-05-07 23:17:31 +0800

    20220507

---
+++ 20220507/test.c
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//int main()
+//{
+//	
+//	return 0;
+//}
+
+//#include<stdio.h>
+//static int n = 555;
+//int* p = (int*)&n;
+//int main()
+//{
+//	//static const int n = 555;
+//	*p = 666;
+//	printf("n=%d\n", n);
+//	printf("*p=%d\n", *p);
+//	return 0;
+//}
